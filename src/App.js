e9976cfe69cfa9ab78784451fea1006eb4c54f21
Victor Gonzalez <dazedbutamused@gmail.com>
2019-10-30 16:57:45 -0700

    created zero handler

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import './App.css';
 
+let title = 'Time For Zeros';
+function zeroChange(){
+  let arr = title.split(' ');
+  for (let i = 0; i < arr.length; i++) {
+    let zeros='';
+    for (let j = 0; j < arr[i].length; j++) {
+      zeros += '0';
+    }
+    arr[i] = zeros;
+  }
+ return this.setState = {title: arr.join('_')};
+};
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Time For Zeros</h1>
+        <h1>{title}</h1>
         <h3> Nothing Matters</h3>
       </header>
       <body>
-        <button>Click Me</button>
+        <button onClick={zeroChange}>Click Me</button>
       </body>
     </div>
   );
