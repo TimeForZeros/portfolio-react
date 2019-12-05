7b721f8a6422b02ae0a4c951580feff03f784737
Victor Gonzalez <dazedbutamused@gmail.com>
2019-12-05 15:29:00 -0800

    Project Sections Styling

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
 import About from "./About/About";
+import Portfolio from './Portfolio/Portfolio';
 import './Body.css';
 
 let Body = props => {
   return (
-    <div>
+    <div >
         <div className="item-container">
           <h1>{props.title}</h1>
           <h3> Nothing Matters</h3>
@@ -13,6 +14,7 @@ let Body = props => {
             <h2> About</h2>
             <About />
             <h2> Projects</h2>
+            <Portfolio />
         </section>
     </div>
   );
---
+++ src/components/Body/Portfolio/Portfolio.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Portfolio.css";
+
+let Portfolio = () => {
+  return (
+    <div>
+      <section className='web-section'>
+        <h3 className="project-header">Web Applications</h3>
+        <h4 className='project-title'>2048</h4>
+        <h4 className='project-title'>What's In My Fridge Foo?</h4>
+        <h4 className='project-title'>My Car Diary</h4>
+        <h4 className='project-title'>Hit Me Up Fam!</h4>
+      </section>
+
+      <section className='animation-section'>
+        <h3 className="project-header">Animation</h3>
+        <h4 className='project-title'>Digital</h4>
+        <h4 className='project-title'>Stop-Motion</h4>
+      </section>
+
+      <section className='digital-media-section'>
+        <h3 className="project-header">Digital Media</h3>
+        <h4 className='project-title'>Adobe Illustrator</h4>
+        <h4 className='project-title'>Adobe Photoshop</h4>
+        <h4 className='project-title'>Adobe InDesign</h4>
+      </section>
+
+    
+    </div>
+  );
+};
+
+export default Portfolio;
