846f324920bd4ae2f5a57e8a8626c2bbbe715cf0
Victor Gonzalez <dazedbutamused@gmail.com>
2019-12-12 20:44:09 -0800

    consistent color theme

--- src/components/Body/Portfolio/Portfolio.jsx
+++ src/components/Body/Portfolio/Portfolio.jsx
@@ -4,7 +4,7 @@ import "./Portfolio.css";
 let Portfolio = () => {
   return (
     <div>
-      <section className='web-section'>
+      <section className='section-header'>
         <h3 className="project-header">Web Applications</h3>
         <h4 className='project-title'>2048</h4>
         <h4 className='project-title'>What's In My Fridge Foo?</h4>
@@ -12,13 +12,13 @@ let Portfolio = () => {
         <h4 className='project-title'>Hit Me Up Fam!</h4>
       </section>
 
-      <section className='animation-section'>
+      <section className='section-header'>
         <h3 className="project-header">Animation</h3>
         <h4 className='project-title'>Digital</h4>
         <h4 className='project-title'>Stop-Motion</h4>
       </section>
 
-      <section className='digital-media-section'>
+      <section className='section-header'>
         <h3 className="project-header">Digital Media</h3>
         <h4 className='project-title'>Adobe Illustrator</h4>
         <h4 className='project-title'>Adobe Photoshop</h4>
